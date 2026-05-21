39ca7493013d27209c16f51969832bca37c6f971
JagerMeistars <cuc18062007v2@gmail.com>
2026-05-22 02:49:33 +0300

    chore: bump version 0.1.0 → 0.1.1 (verify reload)

--- objcubed.js
+++ objcubed.js
@@ -2989,7 +2989,7 @@
         author: 'JagerMeistars, fork of Godlander\'s objmc',
         description: 'Export the current model with obj³ encoding for Minecraft resource packs',
         icon: 'icon',
-        version: '0.1.0',
+        version: '0.1.1',
         min_version: '4.8.0',
         variant: 'desktop',
         onload() {
